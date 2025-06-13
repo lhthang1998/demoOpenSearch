02a95d597ab8cb67782e51c6c279b4da04c13e1f
Lê Hữu Thắng <lhthang.1998@gmail.com>
2025-06-13 15:33:03 +0700

    set up logback

--- src/main/java/com/example/demoOpenSearch/DemoOpenSearchApplication.java
+++ src/main/java/com/example/demoOpenSearch/DemoOpenSearchApplication.java
@@ -1,13 +1,18 @@
 package com.example.demoOpenSearch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoOpenSearchApplication {
-
+	private static final Logger logger
+			= LoggerFactory.getLogger(DemoOpenSearchApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOpenSearchApplication.class, args);
+		logger.info("Hello this is my App");
 	}
 
 }
