1ea47529887a8d5568674a39ca82beb6d26cfffe
LHT <clonelht.it@gmail.com>
2025-06-13 14:42:51 +0700

    first commit

---
+++ src/main/java/com/example/demoOpenSearch/DemoOpenSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoOpenSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoOpenSearch/DemoOpenSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoOpenSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
